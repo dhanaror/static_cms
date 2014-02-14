29ccc4059fd88537328d7ff3e3a24ecf3dd3ddb5
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2014-02-14 11:56:10 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StaticCms::Application.config.secret_key_base = '77cb1126a8924c588f7424bc5dafef9f5f1eab2dab3646fcb169086f56f6dc92fba752b22f29abbfbf82d53d9d2cf2290ae071f17a0ca6d791b9f0edc75b95e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StaticCms::Application.config.session_store :cookie_store, key: '_static_cms_session'
