3c435df38acc441bb5f54115a3cb1d098e744d72
Vijay <vijay@vijay.(none)>
2014-02-20 00:44:21 +0530

    views,models,controller added for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,21 @@
 class ImagesController < ApplicationController
-	def index
-		render :nothing=>true,:layout=>true
+
+	def index		
+		#render :nothing=>true,:layout=>true
 	end
+	def upload_image
+		@image = Image.create(image_params)
+		render :action => "list_image", :layout => true
+	end
+
+	
+	def list_image
+	end	
+
+	private
+	def image_params
+		debugger
+		params.require(params[:image]).permit(:title,:caption,:avatar)
+	end	
+
 end
