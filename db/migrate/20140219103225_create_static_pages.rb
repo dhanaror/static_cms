db3b632dc304239b2bd0b71edfdac68eaa588787
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2014-02-20 19:44:48 +0530

    static page model and migration fiel

---
+++ db/migrate/20140219103225_create_static_pages.rb
@@ -0,0 +1,9 @@
+class CreateStaticPages < ActiveRecord::Migration
+  def change
+    create_table :static_pages do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
