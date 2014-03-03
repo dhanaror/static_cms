83afbbd39c725676afb23439607e3c13f244dd8a
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2014-03-03 17:36:46 +0530

    static page save

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,36 @@
 class StaticPagesController < ApplicationController
+	before_filter :set_static_page,:only=>[:show,:edit,:update,:destroy]
 	def index
+	end
+
+	def show
+	end
+
+	def new
+		@static_page = StaticPage.new
+	end
+
+	def create
+		@static_page = StaticPage.new static_page_params
+		@static_page.save
+		redirect_to @static_page
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+    private
+	def static_page_params
+		params.require(:static_page).permit(:title,:description)
+	end
+
+	def set_static_page
+		@static_page = StaticPage.find params[:id]
 	end
 end
